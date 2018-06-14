ef04ce5c1fbafc7de9c074d20a7c9e9435b7b353
epicderek <lij@winchesterthurston.org>
2018-06-14 16:18:05 +0800

    Deep copy functionality added. Wrapping still errorneous.

--- dermatj/utils/seq/Array.java
+++ dermatj/utils/seq/Array.java
@@ -1,11 +1,21 @@
 package dermatj.utils.seq;
 
+import static dermatj.utils.excs.Exceptions.*; 
+import dermatj.utils.seq.container.map.Maps; 
+import dermatj.utils.seq.container.map.DuoMap;  
+
 public abstract class Array 
 {	
 	/**
-	 * The primitive array types allowed by java. 
+	 * The associated primitive types with their wrapper types. 
 	 */
-	private static final Class<?>[] PMATYPES = new Class<?>[8]; 
+	public static final DuoMap<Class<?>,Class<?>> PMTYPES = new DuoMap<Class<?>,Class<?>>(); 
+	/**
+	 * The associated primitive types with their one dimensional primitive array counterparts. 
+	 */
+	public static final DuoMap<Class<?>,Class<?>> PMATYPES = new DuoMap<Class<?>,Class<?>>(); 
+	
+	
 	/**
 	 * The number of dashes printed when the Array.printDash method is invoked. 
 	 */
@@ -13,9 +23,19 @@ public abstract class Array
 	
 	static
 	{
-		//Load the primitive array classes. 
-		addAll(PMATYPES,0,boolean[].class,byte[].class,char[].class,short[].class,
-						  int[].class,long[].class,float[].class,double[].class);
+		//Load the primitive classes and their wrapper counterparts. 
+		Maps.putAll(PMTYPES,
+				new Class<?>[]
+		{boolean.class,byte.class,char.class,short.class,int.class,long.class,float.class,double.class},
+				new Class<?>[]
+		{Boolean.class,Byte.class,Character.class,Short.class,Integer.class,Long.class,Float.class,Double.class});
+		//Load the primitive classes and their 1D primitive array counterparts. 
+		Maps.putAll(PMATYPES,
+				new Class<?>[] 
+		{boolean.class,byte.class,char.class,short.class,int.class,long.class,float.class,double.class},
+		 		new Class<?>[]
+		{boolean[].class,byte[].class,char[].class,short[].class,int[].class,long[].class,float[].class,double[].class}); 
+		
 	}
 	
 	/**
@@ -36,12 +56,11 @@ public abstract class Array
 	 */
 	public static boolean isPrimitiveArray(Class<?> type)
 	{
-		for(Class<?> holder: PMATYPES)
-			if(holder==type)
+		if(PMATYPES.containsValue(type))
+			return true; 
+		else if(Object[].class.isAssignableFrom(type))
+			if(isPrimitiveArray(type.getComponentType()))
 				return true; 
-			else if(Object[].class.isAssignableFrom(type))
-				if(isPrimitiveArray(type.getComponentType()))
-					return true; 
 		return false; 
 	}
 	
@@ -150,7 +169,6 @@ public abstract class Array
 			return true; 
 		}
 		return false; 
-		
 	}
 	
 	/**
@@ -267,6 +285,23 @@ public abstract class Array
 	}
 	
 	/**
+	 * Fill the given assumed 1D array of Object reference starting from the given starting index 
+	 * with the elements given in the variable argument list of the same type. This method will 
+	 * not generate an exception if the target array is filled before the filling arguments are 
+	 * exhausted. It will only throw an IndexOutOfBoundsException when the filling list is exhausted 
+	 * before the target array is filled. 1D primitive arrays can also be filled in by this method. 
+	 * Prefer the compile time checked version of Array.addAll when the type of array is known. 
+	 * @param arr The 1D array to be filled. 
+	 * @param index The starting index to fill the array. 
+	 * @param fill The argument array to fill the array with starting from its first index. Arguments of wrong types generate exceptions. 
+	 */
+	public static void addAll(Object arr, int index, Object fill)
+	{
+		for(int i=0; i<java.lang.reflect.Array.getLength(arr)-index; i++)
+			java.lang.reflect.Array.set(arr,index+i,java.lang.reflect.Array.get(fill,i));
+	}
+	
+	/**
 	 * Print a dashed line of the number of segments given by the customary public Array.dashNum field. 
 	 */
 	public static void printDash()
@@ -285,9 +320,111 @@ public abstract class Array
 		System.out.println();
 	}
 	
+	/**
+	 * Copy the given object array of any dimensions under object reference to an identical array of the
+	 * same dimensions by copying the references of the most fundamental elements, that is, a deep copy
+	 * is performed. Primitive arrays should be copied by the overloaded method in which the identify
+	 * of the array is veiled under an object reference. 
+	 * @param arr The array to be copied. 
+	 * @return The copied array. 
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T[] copy(T[] arr)
+	{
+		return (T[])copy((Object)arr); 
+	}
+	
+	/**
+	 * Copy the given array of any dimensions under object reference to an identical array of the
+	 * same dimensions by copying the references of the most fundamental elements, that is, a deep copy
+	 * is performed. Primitive arrays should be copied by this method, whereas Object arrays should 
+	 * be copied by overloaded method with compile time type checking. 
+	 * @param arr The array to be copied. 
+	 * @return The copied array. 
+	 */
+	public static Object copy(Object arr)
+	{
+		//The component type of this array. 
+		Class<?> componentType = arr.getClass().getComponentType(); 
+		//The length of this array. 
+		int length = java.lang.reflect.Array.getLength(arr); 
+		Object wrapped = java.lang.reflect.Array.newInstance(componentType,length); 
+		//Base case of a one-dimensional array. 
+		if(componentType.getComponentType()==null)
+			addAll(wrapped,0,arr); 
+		//If nested layers. 
+		else
+			for(int i=0; i<length; i++)
+				java.lang.reflect.Array.set(wrapped,i,copy(java.lang.reflect.Array.get(arr,i)));
+		return wrapped; 
+	}
+	
+	public static void dimensions(Object[] tar)
+	{
+		
+	}
+	
+	/**
+	 * Wrap the given primitive array object of arbitrary dimensions in an array object of the
+	 * same dimensions but of the wrapper type instead of the primitive. The returned array 
+	 * should be casted by the user if the compile time reference is significant. Warnings, if
+	 * generated, should be suppressed with knowledge of the correctness of this method. This 
+	 * method is implemented with reflection for generality; if minute efficiency is demanded, 
+	 * with the proper knowledge of the peculiar type of the array, wrap that array manually. 
+	 * This method first checks whether the given object is indeed an instance of a primitive 
+	 * array. If not, an Exceptions.NonePrimitiveArrayException is thrown. 
+	 * @param obj The primitive array of some dimensions to be wrapped in an array of the same dimensions of the wrapper type. 
+	 * @return The array object of the wrapper type correspondent to the primitive type of the array given. 
+	 */
+	public static Object wrap(Object obj)
+	{
+		if(!isPrimitiveArray(obj))
+			throw new NonePrimitiveArrayException("The Given Argument is not a Primitive Array!");  
+		return wrapping(obj); 
+	}
+	
+	/**
+	 * Wrap the given primitive array object of arbitrary dimensions in an array object of the
+	 * same dimensions but of the wrapper type instead of the primitive without first checking 
+	 * if the given object is a primitive array. The returned array should be casted by the user 
+	 * if the compile time reference is significant. Warnings, if generated, should be suppressed 
+	 * with knowledge of the correctness of this method. This method is implemented with reflection 
+	 * for generality; if minute efficiency is demanded, with the proper knowledge of the peculiar
+	 * type of the array, wrap that array manually. 
+	 * @param obj The primitive array of some dimensions to be wrapped in an array of the same dimensions of the wrapper type. 
+	 * @return The array object of the wrapper type correspondent to the primitive type of the array given. 
+	 */
+	private static Object wrapping(Object obj)
+	{
+		//Type of this array. 
+		Class<?> type = obj.getClass(); 
+		//The superficial length of this array, the dimension of this level.  
+		int length = java.lang.reflect.Array.getLength(obj); 
+		Object wrapped;
+		//Base case of a 1D primitive array. 
+		if(PMATYPES.containsValue(type))
+		{
+			//Create and fill the 1D primitive array into its wrapper type. 
+			wrapped = java.lang.reflect.Array.newInstance(PMTYPES.get(type.getComponentType()),length); 
+			//Fill. 
+			addAll(wrapped,0,obj); 
+			return wrapped; 
+		}
+		//Still layers nested. 
+		wrapped = java.lang.reflect.Array.newInstance(type.getComponentType(),length); 
+		//Access each sub-array and recursively solve for each such array. 
+		for(int i=0; i<length; i++)
+			java.lang.reflect.Array.set(wrapped,i,wrapping(java.lang.reflect.Array.get(obj,i))); 
+		return wrapped; 
+	}
+	
+	
+	
+	
 	public static void main(String[] args) 
 	{	
-		printline(isPrimitiveArray(new Object[]{})); 
+		//printline(wrap(new int[][]{{1,2,3,4,5}}));
+		printArray(copy(new Integer[][]{{1,2,3,4,5}}));
 	}
 
 }
